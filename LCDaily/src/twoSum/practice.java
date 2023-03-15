976f9180da00c57e29b286bcb45a086bc32d6de5
KUNQI WAN <56388431+kunqiwan@users.noreply.github.com>
2023-03-16 00:51:07 +0100

    practical question
    use hashmap

---
+++ LCDaily/src/twoSum/practice.java
@@ -0,0 +1,56 @@
+package twoSum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+//Write a function:
+//
+//class Solution { public int solution(int[] A); }
+//
+//that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+//
+//For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+//
+//Given A = [1, 2, 3], the function should return 4.
+//
+//Given A = [−1, −3], the function should return 1.
+//
+//Write an efficient algorithm for the following assumptions:
+//
+//N is an integer within the range [1..100,000];
+//each element of array A is an integer within the range [−1,000,000..1,000,000].
+public class practice {
+
+    public static void main(String[] args){
+        int [] s = {1,1,100,4};
+        System.out.println(Solution.solution(s));
+
+    }
+
+    static class Solution {
+        public static int solution(int[] A) {
+            int N = A.length;
+            System.out.println("N"+N);
+            // start from 1, a size 4 array the maximum is 4,so the seen array size should be 5, we jump 0
+            boolean[] seen = new boolean[N+1];
+            System.out.println(seen.length);
+            System.out.println("show:"+seen[3]);
+            for (int i = 0; i < N; i++) {
+                if (A[i] > 0 && A[i] <=N) {
+                    seen[A[i]] = true;
+                }
+                System.out.println("i"+i);
+            }
+
+            for (int i = 1; i <N; i++) {
+                if (!seen[i]) {
+                    return i;
+                }
+            }
+
+            return N;
+        }
+    }
+}
