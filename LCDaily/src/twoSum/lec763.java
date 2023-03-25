e03f7933ce43754434aac67eb52f5dd0df5bcdb6
KUNQI WAN <56388431+kunqiwan@users.noreply.github.com>
2023-03-25 22:12:44 +0100

    greedy ,update every iterate

---
+++ LCDaily/src/twoSum/lec763.java
@@ -0,0 +1,16 @@
+package twoSum;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class lec763 {
+
+    public List<Integer> partitionLabels(String s) {
+        int n =s.length();
+        Map<Character, Integer> letterMap = new HashMap<Character, Integer>();
+        for (int i=0;i<n;i++){
+            
+        }
+    }
+}
