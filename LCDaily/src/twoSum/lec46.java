2ef8370bea3e5e78ed82420bf6908016188a7375
KUNQI WAN <56388431+kunqiwan@users.noreply.github.com>
2023-03-27 20:49:21 +0200

    Backtracking - Permutation

--- LCDaily/src/twoSum/lec46.java
+++ LCDaily/src/twoSum/lec46.java
@@ -1,24 +1,47 @@
 package twoSum;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class lec46 {
-//back tracking
-//    public static long factorial(int n) {
-//        if (n == 0) {
-//            return 1;
-//        } else {
-//            return n * factorial(n-1);
-//        }
-//    }
-//
-//    public List<List<Integer>> permute(int[] nums) {
-//    int n =nums.length;
-//    int factorial = (int) factorial(n);
-//    int[] [] p = new int[factorial][];
-//    for(int i=0;i<n;i++){
-//
-//    }
-//
-//    }
+
+//create result to store each result of path
+List<List<Integer>> result = new LinkedList<>();
+//store one possible permutation
+LinkedList<Integer> path = new LinkedList<>();
+//to determine this number has been traversed or not
+boolean [] used;
+
+//main function to execute recursion
+public List<List<Integer>> permute(int[] nums) {
+if (nums.length == 0){
+    return result;
+}
+    used = new boolean[nums.length];
+ backtracking(nums);
+    return result;
+}
+
+    //back tracking
+void backtracking (int[] nums){
+if (path.size() ==nums.length ){
+result.add(path);
+return;
+}
+//single structure logistic
+for (int i=0;i<nums.length;i++){
+    if(used[i]){
+        continue;
+    }
+    path.add(nums[i]);
+    used[i] = true;
+    backtracking(nums);
+    used[i] = false;
+    path.removeLast();
+
+}
+
+
+}
 }
