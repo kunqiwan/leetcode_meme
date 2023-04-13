c185a17b08cea32ce220e505fb6494564dd08cf5
KUNQI WAN <56388431+kunqiwan@users.noreply.github.com>
2023-04-13 12:17:43 +0200

    Two pointer, find the end point

--- LCDaily/src/twoSum/lec2485.java
+++ LCDaily/src/twoSum/lec2485.java
@@ -2,6 +2,25 @@ package twoSum;
 
 public class lec2485 {
     public int pivotInteger(int n) {
-    return 0;
+    int left =1;
+    int right = n;
+    int sum_left =1;
+    int sum_right =right;
+    while(left!=right){
+
+    if(sum_left >= sum_right){
+        right--;
+        sum_right = sum_right+right;
+    } else{
+        left++;
+        sum_left = sum_left+left;
+    }
+    }
+    if (sum_left == sum_right){
+        return left;
+    } else {
+        return -1;
+
+    }
     }
 }
