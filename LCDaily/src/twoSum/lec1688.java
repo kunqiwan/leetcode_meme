1d057138c293a15bf3d6d1192a8be8d7a7dcac8d
KUNQI WAN <56388431+kunqiwan@users.noreply.github.com>
2023-04-15 21:12:10 +0200

    odd even rule

---
+++ LCDaily/src/twoSum/lec1688.java
@@ -0,0 +1,18 @@
+package twoSum;
+
+public class lec1688 {
+    public int numberOfMatches(int n) {
+        int matches =0;
+        while(n!=1){
+            if(n%2==0){
+                matches = matches+n/2;
+                n = n/2;
+            } else{
+                matches =matches+(n-1)/2;
+                n = (n-1)/2+1;
+
+            }
+        }
+    return matches;
+    }
+}
