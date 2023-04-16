3d52ebe60341f4fb8dc11b35f7152ad6934f9dc5
KUNQI WAN <56388431+kunqiwan@users.noreply.github.com>
2023-04-16 14:34:16 +0200

    Interval Regularity

---
+++ LCDaily/src/twoSum/lec1450.java
@@ -0,0 +1,14 @@
+package twoSum;
+
+public class lec1450 {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int n =startTime.length;
+        int count =0;
+        for(int i=0;i<n;i++){
+            if(startTime[i]<=queryTime && endTime[i]>=queryTime){
+                count++;
+            }
+        }
+        return count;
+    }
+}
