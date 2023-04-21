a9dee89d6fcbfe54be41b0772de23b683d8a1b97
KUNQI WAN <56388431+kunqiwan@users.noreply.github.com>
2023-04-21 19:19:49 +0200

    Constructor, member variables

---
+++ LCDaily/src/twoSum/lec1603.java
@@ -0,0 +1,29 @@
+package twoSum;
+
+public class lec1603 {
+    int small;
+    int medium;
+    int big;
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.small = small;
+        this.medium = medium;
+        this.big = big;
+
+    }
+
+    public boolean addCar(int carType) {
+        if(carType==3){
+            small--;
+            return small>=0;
+        }
+        else if(carType==2){
+            medium--;
+            return medium>=0;
+        }
+        else{
+            big--;
+            return big>=0;
+        }
+    }
+}
